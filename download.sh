2c08942863d987e1e8a0987f518c14306dcfeeb1
hautph <hautph@gmail.com>
2018-06-04 20:56:06 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-./Fshare https://www.fshare.vn/file/YJHMDQ9U6Z9Z
-./Fshare https://www.fshare.vn/file/96TEAU89CMU3
-./Fshare https://www.fshare.vn/file/Q7IPQC5GMH9L
+./Fshare.py https://www.fshare.vn/file/YJHMDQ9U6Z9Z
+./Fshare.py https://www.fshare.vn/file/96TEAU89CMU3
+./Fshare.py https://www.fshare.vn/file/Q7IPQC5GMH9L
 
 # Có thể thêm nhiều dòng
